52d81073d7574d120e25d1454cf0de0c2d2e5f2a
Your NameDarius <Dariusalexander1118@gmail.com>
2025-09-03 16:08:47 -0700

    push

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -22,8 +22,6 @@ export const upsertUserProgress = async (courseId: number) => {
         throw new Error("Course not found");
     }
 
-    
-
     // TODO: Enable once units and lessons are added
     // if(!course.units.length || !course.units[0].lessons.length) {
     //    throw new Error("Course is empty");
