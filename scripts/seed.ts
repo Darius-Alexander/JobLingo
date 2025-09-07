8f8d1155de5086c9934cc49cd69b37918d7c1308
Darius Alexander <your_github_email@example.com>
2025-09-07 16:03:34 -0700

    units

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,11 @@ const main = async () => {
 
         await db.delete(schema.courses);
         await db.delete(schema.userProgress);
+        await db.delete(schema.units);
+        await db.delete(schema.lessons);
+        await db.delete(schema.challenges);
+        await db.delete(schema.challengeOptions);
+        await db.delete(schema.challengeProgress);
 
         await db.insert(schema.courses).values([
             {
@@ -40,6 +45,55 @@ const main = async () => {
                 id: 5,
                 title: "Chinese",
                 imageSrc: "/cn.svg",             
+            },
+        ]);
+
+        await db.insert(schema.units).values([
+            {
+                id: 1,
+                courseId: 1,
+                title: "Unit 1",
+                description: "Learn the basics of French",
+                order: 1,
+            },
+        ]);
+
+        await db.insert(schema.lessons).values([
+            {
+                id: 1,
+                unitId: 1, // Unit 1 (Learn the basics)
+                order: 1,
+                title: "Nouns",
+            },
+        ])
+
+        await db.insert(schema.challenges).values([
+            {
+                id: 1,
+                lessonId: 1, // Nouns
+                type: "SELECT",
+                order: 1,
+                question: 'Which one of these is a "bank"?',
+            },
+            
+        ]);
+
+        await db.insert(schema.challengeOptions).values([
+            {
+                id: 1,
+                challengeId: 1,
+                imageSrc: "/bank.svg",
+                correct: true,
+                text: "banque",
+                audioSrc: "/fr_bank.mp3"
+            },
+            {
+                id: 2,
+                challengeId: 1,
+                imageSrc: "/banker.svg",
+                correct: false,
+                text: "un banquier",
+                audioSrc: "/fr_banker.mp3"
             },
         ]);
 
